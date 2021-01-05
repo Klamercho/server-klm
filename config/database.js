66e47ffaeecf69f230195fb8a5e077b378cafd94
Dimitrov <kamen.dimitrov1@hp.com>
2021-01-05 17:10:33 +0200

    added a line in the database connection file

--- config/database.js
+++ config/database.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 module.exports = () => {
     return mongoose.connect(process.env.MONGODB_URI || config.dbConnectionString, {
         useNewUrlParser: true, 
+        useCreateIndex: true,
         useUnifiedTopology: true
     }).then((data) => {
         console.log("Connected to database!");
